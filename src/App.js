c5fdd942361b4c5028c7f1b08ec7c6ed9a8a5df4
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-10-11 21:08:27 +0300

    feat: useMemo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, {useRef} from "react";
+import React, {useMemo, useState} from "react";
 function App() {
-    const inputRef = useRef(null);
+    const [state, setState] = useState(1);
+    const [price, setPrice] = useState(10);
+    const [count, setCount] = useState(5);
 
-    const focusInput = () => {
-        inputRef.current.focus();
+    function hardCalc(price, count) {
+        console.log("On");
+        return price * count;
     }
 
-  return (
-      <div>
-          <input ref={inputRef} type="text"/>
-          <button onClick={focusInput}>Фокус на поле вводу</button>
-      </div>
-  );
+    const hardRes = useMemo(() => hardCalc(price, count), [price, count]);
+
+    return (
+        <div>
+            <p>State {state}</p>
+            <p>Price {price}</p>
+            <p>Count {count}</p>
+            <p>Res: {hardRes}</p>
+            <button onClick={() => setState(state + 1)}>Update state</button>
+            <button onClick={() => setPrice(price + 1)}>Update state</button>
+            <button onClick={() => setCount(count + 1)}>Update state</button>
+        </div>
+    );
 }
 
 export default App;
